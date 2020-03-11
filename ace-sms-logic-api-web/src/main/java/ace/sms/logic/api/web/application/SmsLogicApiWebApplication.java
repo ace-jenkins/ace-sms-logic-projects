265509dd9f9b671d9e0a4e8bb0c14f6447068ee7
Casapr <279397942@qq.com>
2020-03-11 09:49:28 +0800

    添加自动化配置服务发现

--- ace-sms-logic-api-web/src/main/java/ace/sms/logic/api/web/application/SmsLogicApiWebApplication.java
+++ ace-sms-logic-api-web/src/main/java/ace/sms/logic/api/web/application/SmsLogicApiWebApplication.java
@@ -1,5 +1,6 @@
 package ace.sms.logic.api.web.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,8 +15,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class SmsLogicApiWebApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(SmsLogicApiWebApplication.class, args);
+
     }
 }
